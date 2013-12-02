3e0d38d63bd41cc4809133182a2d717e6f0d6d87
medieroed2 <github@castlib.dk>
2013-12-02 15:27:51 +0100

    doing shit again

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 function doShit(){
     return "No shit - doing shit";
 }
+
+function doShitAgain(){
+    return "No shit - doing shit again";
+}
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
